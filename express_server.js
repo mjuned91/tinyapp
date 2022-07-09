5310070ad32fefd261cab47aeea0ebc827d54f83
mjuned <mmj.nowaz@gmail.com>
2022-07-09 07:00:24 +0000

    Transfer helper functions to separate helpers.js file and export. Add helpers.js require to express_server.js

--- express_server.js
+++ express_server.js
@@ -1,35 +1,10 @@
 const express = require("express");
 const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
+const { generateRandomString, getUserByEmail, urlsForUser } = require("./helpers");
 const app = express();
 const PORT = 8080;
 
-const generateRandomString = () => {
-  const alphaNumerical = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let result = '';
-  for (let i = 0; i < 6; i++) {
-    result += alphaNumerical.charAt(Math.floor(Math.random() * alphaNumerical.length));
-  }
-  return result;
-};
-
-const getUserByEmail = (email, database) => {
-  for (const user in database) {
-    if (database[user].email === email) {
-      return database[user];
-    };
-  };
-};
-
-const urlsForUser = (id, database) => {
-  let userURLs = {};
-  for (const shortURL in database) {
-    if (database[shortURL].userID === id) {
-      userURLs[shortURL] = database[shortURL];
-    };
-  };
-  return userURLs;
-};
 
 const urlDatabase = {
   "b6UTxQ": {
