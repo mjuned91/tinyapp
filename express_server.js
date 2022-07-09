85e1d2e71adab14f172d44a3259b8a251d24c3d4
mjuned <mmj.nowaz@gmail.com>
2022-07-09 07:36:26 +0000

    Update variables

--- express_server.js
+++ express_server.js
@@ -6,29 +6,8 @@ const app = express();
 const PORT = 8080;
 
 
-const urlDatabase = {
-  "b6UTxQ": {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
-  },
-  "i3BoGr": {
-    longURL: "http://www.google.com",
-    userID: "aJ48lW"
-  }
-};
-
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
+const urlDatabase = {};
+const users = {};
 
 app.set("view engine", "ejs");
 
